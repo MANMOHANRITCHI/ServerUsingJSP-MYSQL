6603d9043a9f123a3133a82f7105bb10683125f2
MANMOHANRITCHI <36854616+MANMOHANRITCHI@users.noreply.github.com>
2022-02-23 19:25:03 +0530

    Add files via upload

---
+++ Registration.sql
@@ -0,0 +1,6 @@
+use project;
+create table users(id int primary key auto_increment,uname varchar(50),upwd varchar(50),uemail varchar(70) ,umobile varchar(20));
+desc users;
+
+select * from users;
+
